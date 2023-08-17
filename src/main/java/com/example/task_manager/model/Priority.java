7c88d6632d611e66c1d9a62228860c146af9a536
Chris G <12830070+gruemme@users.noreply.github.com>
2023-08-17 11:40:48 +0200

    Added enitites

---
+++ src/main/java/com/example/task_manager/model/Priority.java
@@ -0,0 +1,7 @@
+package com.example.task_manager.model;
+
+public enum Priority {
+  LOW,
+  NORMAL,
+  URGENT
+}
---
+++ src/main/java/com/example/task_manager/model/Task.java
@@ -0,0 +1,49 @@
+package com.example.task_manager.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+  @Column(nullable = false, unique = true)
+  private String name;
+  private boolean done;
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Instant created;
+  @Enumerated(EnumType.STRING)
+  private Priority priority;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public Instant getCreated() {
+    return created;
+  }
+
+  public Priority getPriority() {
+    return priority;
+  }
+
+  @Override
+  public String toString() {
+    return "Task{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
+  }
+}
